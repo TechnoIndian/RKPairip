91904435dff51abc22e689680ae76dc2f0bd4e9b
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-11-21 13:31:31 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-	long_description = fh.read()
+    long_description = fh.read()
 
 setuptools.setup(
-	name="RKPairip",
-	version="5.0",
-	author="RK_TECHNO_INDIA",
-	author_email="TechnoIndia786@gmail.com",
-	description="Remove Pairip Protection with RKPairip Script & Recover String & Rebuild Apk",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/Technoindian/RKPairip",
-	packages=setuptools.find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	python_requires='>=3.12',
-	entry_points={
-		'console_scripts': [
-			'RKPairip=RKPairip.RKPairip:main',
-		],
-	},
+    name="rkjp",
+    version="1.0",
+    author="RK_TECHNO_INDIA",
+    author_email="TechnoIndia786@gmail.com",
+    description="Recover String & Rebuild Apk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/Technoindian/RKPairip",
+    keywords='RKPairip',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    entry_points={
+        'console_scripts': [
+            'RKPairip=RKPairip.__main__:RK_Techno_IND',
+        ],
+    },
 )

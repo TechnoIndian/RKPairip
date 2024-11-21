c58f6819f71bfd1c3794fd730a002417f743ec9a
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-11-21 22:28:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rkjp",
-    version="2.0",
+    version="2.0.0",
     author="RK_TECHNO_INDIA",
     author_email="TechnoIndia555@gmail.com",
     description="Recover String & Rebuild Apk",
